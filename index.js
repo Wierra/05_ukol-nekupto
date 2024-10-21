cfcd821e54ee179820e4203a278c4912f4980795
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-21 23:15:37 +0200

    Řešení příkladu 02

--- index.js
+++ index.js
@@ -27,3 +27,27 @@ product2Button.classList.remove("btn-primary")
 /*Do další proměnné si uložte element s třídou card-title posledního produktu. Pomocí voláni metody classList.toggle přidejte na tento element třídu text-center. Text by se měl tímto zarovnat na střed. Vyzkoušejte si, že když tuto metodu zavoláte znova, třída se z prvku odstraní. Takto můžete přepínat mezi přidáním a odebráním třídy pomocí opakovaného volání této metody.*/
 const card3 = document.querySelector("#product3 .card-title")
 card3.classList.toggle("text-center")
+
+
+const product1HTML = `
+<img class="card-img-top" src="images/destniklobouk.jpg" alt="Card image cap">
+<div class="card-body">
+    <h2 class="card-title">Deštníklobouk</h2>
+    <p class="card-text">Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.</p>
+</div>
+    <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`
+
+const elProduct1 = {
+  image: "images/anatoplavky.jpg",
+  name: "Divno muž",
+  description: "Nečekaná přeháňka? No problem, vás už nikdy nezaskočí a nepřekvapí...",
+  button: "Nekup to!"
+}
+
+const cardProduct1 = document.querySelector("#product1")
+cardProduct1.innerHTML = `
+<img class="card-img-top">${elProduct1.image}
+<h2 class="card-title">${elProduct1.name}</h2>
+<p class="card-text">${elProduct1.description}</p>
+<button class="btn btn-lg btn-primary btn-block">${elProduct1.button}</butoon>
+`
