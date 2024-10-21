94439a38ea1ce64d4cba97a35853d0d973f776ad
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-21 22:48:48 +0200

    Řešení příkladu

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+/*2. V souboru index.js si na posledním řádku do proměnné uložte kartu s prvním produktem. Pomocí metody classList.add přidejte na tento element třídu border-primary, abychom první produkt na stránce zvýraznili.*/
+const card1 = document.querySelector("#product1")
+card1.classList.add("border-primary")
+
+/*Do jiné proměnné si uložte tlačítko na druhé kartě. Pomocí metody classList.remove odeberte třídu btn-primary a podívejte se, jak se tlačítko vizuálně změnilo.*/
+const product2Button = document.querySelector("#product2 button")
+product2Button.classList.remove("btn-primary")
+
+/*Do další proměnné si uložte element s třídou card-title posledního produktu. Pomocí voláni metody classList.toggle přidejte na tento element třídu text-center. Text by se měl tímto zarovnat na střed. Vyzkoušejte si, že když tuto metodu zavoláte znova, třída se z prvku odstraní. Takto můžete přepínat mezi přidáním a odebráním třídy pomocí opakovaného volání této metody.*/
+const card3 = document.querySelector("#product3 .card-title")
+card3.classList.toggle("text-center")
